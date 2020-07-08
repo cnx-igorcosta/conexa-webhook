390b7d9e2877bcb3b257e105470a790972806bed
Igor <igor.costa@conexasaude.com.br>
2020-07-08 12:59:34 -0300

    projeto padrao

---
+++ app.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+
+const app = express()
+
+// Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get('/callback', (req, res) => {
+    const { body, query } = req
+    console.log(JSON.stringify(body))
+    console.log(JSON.stringify(query))
+    return res.end()
+})
+
+module.exports = app
